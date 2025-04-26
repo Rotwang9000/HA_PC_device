1b515454009e3379fef590936c506b992e0bb51f
S. K. Rotwang 9000 <144210384+Rotwang9000@users.noreply.github.com>
2025-04-26 12:46:12 +0200

    Update config_flow.py

--- custom_components/pc/config_flow.py
+++ custom_components/pc/config_flow.py
@@ -1,6 +1,15 @@
 import voluptuous as vol
 from homeassistant import config_entries
-from .const import DOMAIN, CONF_DEVICE_NAME, CONF_FS_NAME, CONF_USE_FS_LOCK
+from .const import (
+    DOMAIN, CONF_DEVICE_NAME, CONF_FS_NAME,
+    CONF_POWER_ON_ACTION, CONF_POWER_OFF_ACTION,
+    CONF_ENFORCE_LOCK,
+    POWER_ON_ACTIONS, POWER_OFF_ACTIONS
+)
+
+# Remove Family Safety options from actions
+POWER_ON_ACTIONS.remove("fs_unlock")
+POWER_OFF_ACTIONS.remove("fs_lock")
 
 class PCConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for PC Device."""
@@ -16,7 +25,9 @@ class PCConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                     {
                         vol.Required(CONF_DEVICE_NAME): str,
                         vol.Optional(CONF_FS_NAME): str,
-                        vol.Optional(CONF_USE_FS_LOCK, default=False): bool,
+                        vol.Required(CONF_POWER_ON_ACTION, default=POWER_ON_POWER): vol.In(POWER_ON_ACTIONS),
+                        vol.Required(CONF_POWER_OFF_ACTION, default=POWER_OFF_POWER): vol.In(POWER_OFF_ACTIONS),
+                        vol.Optional(CONF_ENFORCE_LOCK, default=False): bool,
                     }
                 ),
             )
@@ -24,7 +35,9 @@ class PCConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         # Validate the input
         device_name = user_input[CONF_DEVICE_NAME]
         fs_name = user_input.get(CONF_FS_NAME, "")
-        use_fs_lock = user_input.get(CONF_USE_FS_LOCK, False)
+        power_on_action = user_input[CONF_POWER_ON_ACTION]
+        power_off_action = user_input[CONF_POWER_OFF_ACTION]
+        enforce_lock = user_input.get(CONF_ENFORCE_LOCK, False)
 
         # Ensure unique device name
         await self.async_set_unique_id(device_name.lower())
@@ -35,6 +48,8 @@ class PCConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             data={
                 CONF_DEVICE_NAME: device_name,
                 CONF_FS_NAME: fs_name,
-                CONF_USE_FS_LOCK: use_fs_lock,
+                CONF_POWER_ON_ACTION: power_on_action,
+                CONF_POWER_OFF_ACTION: power_off_action,
+                CONF_ENFORCE_LOCK: enforce_lock,
             },
         )
