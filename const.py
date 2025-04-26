37d313fc0c63b738e286b9b00e16908805928d6d
S. K. Rotwang 9000 <144210384+Rotwang9000@users.noreply.github.com>
2025-04-26 09:07:26 +0200

    Create const.py

---
+++ const.py
@@ -0,0 +1,14 @@
+DOMAIN = "pc"
+
+# Services
+SERVICE_SET_VOLUME = "set_volume"
+SERVICE_MUTE = "mute"
+SERVICE_LOCK = "lock"
+
+# Attributes
+ATTR_VOLUME_LEVEL = "volume_level"
+ATTR_ACTIVE_WINDOW = "activewindow"
+ATTR_SESSION_STATE = "sessionstate"
+
+# MQTT Topics
+CONF_DEVICE_NAME = "device_name"
