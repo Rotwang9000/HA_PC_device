e28a578cdf8b2f0acd7f54b774174de77bb4432c
S. K. Rotwang 9000 <144210384+Rotwang9000@users.noreply.github.com>
2025-04-27 22:03:33 +0200

    Update const.py

--- custom_components/pc/const.py
+++ custom_components/pc/const.py
@@ -4,7 +4,6 @@ DOMAIN = "pc"
 SERVICE_SET_VOLUME = "set_volume"
 SERVICE_MUTE = "mute"
 SERVICE_LOCK = "lock"
-SERVICE_ENFORCE_LOCK = "enforce_lock"
 
 # Attributes
 ATTR_VOLUME_LEVEL = "volume_level"
@@ -13,14 +12,8 @@ ATTR_SESSION_STATE = "sessionstate"
 
 # Configuration
 CONF_DEVICE_NAME = "device_name"
-CONF_FS_NAME = "fs_name"
 CONF_POWER_ON_ACTION = "power_on_action"
 CONF_POWER_OFF_ACTION = "power_off_action"
-CONF_ENFORCE_LOCK = "enforce_lock"
-
-# Family Safety Switches (no longer used, but kept for reference)
-FS_BLOCK_WINDOWS = "block_windows"
-FS_BLOCK_XBOX = "block_xbox"
 
 # Power On/Off Actions
 POWER_ON_POWER = "power_on"
