a69c62354416bdf9aa051a83a6e6d4f047852401
S. K. Rotwang 9000 <144210384+Rotwang9000@users.noreply.github.com>
2025-04-27 21:29:32 +0200

    Update const.py

--- custom_components/pc/const.py
+++ custom_components/pc/const.py
@@ -4,7 +4,7 @@ DOMAIN = "pc"
 SERVICE_SET_VOLUME = "set_volume"
 SERVICE_MUTE = "mute"
 SERVICE_LOCK = "lock"
-SERVICE_ENFORCE_LOCK = "enforce_lock"  # New service to toggle enforced lock
+SERVICE_ENFORCE_LOCK = "enforce_lock"
 
 # Attributes
 ATTR_VOLUME_LEVEL = "volume_level"
@@ -16,7 +16,7 @@ CONF_DEVICE_NAME = "device_name"
 CONF_FS_NAME = "fs_name"
 CONF_POWER_ON_ACTION = "power_on_action"
 CONF_POWER_OFF_ACTION = "power_off_action"
-CONF_ENFORCE_LOCK = "enforce_lock"  # New option for enforced lock
+CONF_ENFORCE_LOCK = "enforce_lock"
 
 # Family Safety Switches (no longer used, but kept for reference)
 FS_BLOCK_WINDOWS = "block_windows"
@@ -25,11 +25,9 @@ FS_BLOCK_XBOX = "block_xbox"
 # Power On/Off Actions
 POWER_ON_POWER = "power_on"
 POWER_ON_WAKE = "wake"
-POWER_ON_FS_UNLOCK = "fs_unlock"
-POWER_ON_ACTIONS = [POWER_ON_POWER, POWER_ON_WAKE, POWER_ON_FS_UNLOCK]
+POWER_ON_ACTIONS = [POWER_ON_POWER, POWER_ON_WAKE]
 
-POWER_OFF_FS_LOCK = "fs_lock"
 POWER_OFF_POWER = "power_off"
 POWER_OFF_HIBERNATE = "hibernate"
 POWER_OFF_SLEEP = "sleep"
-POWER_OFF_ACTIONS = [POWER_OFF_FS_LOCK, POWER_OFF_POWER, POWER_OFF_HIBERNATE, POWER_OFF_SLEEP]
+POWER_OFF_ACTIONS = [POWER_OFF_POWER, POWER_OFF_HIBERNATE, POWER_OFF_SLEEP]
